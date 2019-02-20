47e3724013e56cb77c4984aa11e0ab46d47f81c3
Aredu <aredu_r89@hotmail.com>
2019-02-19 22:06:44 -0300

    11.Taking the data out of the views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,36 @@
 /* GET 'home' page */
 module.exports.homelist = function(req, res){
-    res.render('locations-list', { title: 'Localizador' });
+    res.render('locations-list', { 
+        title: 'Localizador',
+        pageHeader: {
+            title: 'Localizador',
+            strapline: 'Encontrá lugares para trabajar con wifi cerca tuyo!'
+        },
+        sidebar: 'Buscando wifi? Localizador te ayuda a encontrar lugares para trabajar cuando estás fuera de casa.',
+        locations: [
+            {
+                name: 'Starcups',
+                address: 'Av Velez Sarsfield 555, Córdoba',
+                rating: 2,
+                facilities: ['Bebidas calientes','Premium wifi','Tragos'],
+                distance: '100m'
+            },
+            {
+                name: 'Temple Bar',
+                address: 'Bario Güemes, Córdoba',
+                rating: 3,
+                facilities: ['Premium wifi','Tragos'],
+                distance: '200m'
+            },
+            {
+                name: 'Patio Olmos',
+                address: 'Av Velez Sarsfield y San Juan, Córdoba',
+                rating: 4,
+                facilities: ['Comidas','Premium wifi','Helados'],
+                distance: '300m'
+            },
+        ]
+    });
 };
 
 /* GET 'Location info' page */
