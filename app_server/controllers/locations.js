394020e1da70a8c16547354a90ba5f65e5a91896
Aredu <aredu_r89@hotmail.com>
2019-02-20 21:49:44 -0300

    12.Taking the data out from all of the views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -35,10 +35,66 @@ module.exports.homelist = function(req, res){
 
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res){
-    res.render('location-info', { title: 'Detalles' });
+    res.render('location-info', { 
+        title: 'Detalles',
+        pageHeader: {
+            title: 'Starcups'
+        },
+        sidebar: {
+            context: ' está en Localizador porque tiene acceso a wi fi y espacio para sentarse con tu laptop y trabajar.',
+            callToAction: 'Si estuviste en este lugar y te gustó - o si no - por favor dejanos tus comentarios para ayudar a otras personas como vos.'
+        },
+        location: {
+            name: 'Starcups',
+            address: 'Av Velez Sarsfield 555, Córdoba',
+            rating: 3,
+            facilities: ['Bebidas calientes','Premium wifi','Tragos'],
+            coords: {
+                lat: '51.455041',
+                long: '-0.9690884'
+            },
+            openingTimes: [
+                {
+                    days: 'Lunes - Viernes',
+                    opening: '7:00 am',
+                    closing: '10 pm',
+                    closed: false
+                },
+                {
+                    days: 'Sabados',
+                    opening: '8:00 am',
+                    closing: '7 pm',
+                    closed: false
+                },
+                {
+                    days: 'Domingo',
+                    closed: true
+                }
+            ],
+            reviews: [
+                {
+                    author: 'Ariel Rosales',
+                    rating: 3,
+                    timeStamp: '17 Febrero 2019',
+                    reviewText: 'Las instalaciones son muy comodas y tiene un buen wi fi.'
+                },
+                {
+                    author: 'Enzo Mariani',
+                    rating: 4,
+                    timeStamp: '13 Febrero 2019',
+                    reviewText: 'El café no es muy bueno, pero el lugar es cómodo y tranquilo para trabajar.'
+                }
+            ]
+        }
+    });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res){
-    res.render('location-review-form', { title: 'Agregar Comentario' });
+    res.render('location-review-form', { 
+        title: 'Agregar Comentario',
+        pageHeader: {
+            title: 'Comentario sobre Starcups'
+        }
+    });
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET 'about' page */
 module.exports.about = function(req, res){
-    res.render('generic-text', { title: 'Nosotros' });
+    res.render('generic-text', { 
+        title: 'Nosotros',
+        text: 'Localizador fué creado para ayudar a la gente a encontrar lugares para sentarse y trabajar en sus laptops.\n\nLa idea surgió al estudiar nuevas tecnologías de desarrollo web.\n\n'
+    });
 };
